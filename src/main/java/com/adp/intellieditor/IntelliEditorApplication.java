601a3eb9f4c0c272546c9072613b55d2dd8fdd53
Aditya Reddy <adityaravikiran.reddy@adp.com>
2017-06-19 17:22:01 +0530

    Initial commit

---
+++ src/main/java/com/adp/intellieditor/IntelliEditorApplication.java
@@ -0,0 +1,12 @@
+package com.adp.intellieditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelliEditorApplication.class, args);
+	}
+}
