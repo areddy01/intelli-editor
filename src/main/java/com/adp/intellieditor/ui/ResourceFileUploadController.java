c10c3edb69b7c95cf612df747bf8c5ec10a74f99
Aditya Reddy <adityaravikiran.reddy@adp.com>
2017-06-19 19:11:39 +0530

    Added Apache common dependencies File Upload functionality

--- src/main/java/com/adp/intellieditor/ui/ResourceFileUploadController.java
+++ src/main/java/com/adp/intellieditor/ui/ResourceFileUploadController.java
@@ -1,11 +1,14 @@
 package com.adp.intellieditor.ui;
 
-import org.springframework.util.StringUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
 
 /**
  * Created by reddyadi on 6/19/2017.
@@ -15,9 +18,42 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/upload")
 public class ResourceFileUploadController {
 
+    private static String UPLOADED_FOLDER = "./tmp/";
+
     @RequestMapping(method = RequestMethod.POST)
-    public String multiFileUpload(@RequestParam("files") MultipartFile[] files) {
-        return "";
+    public String bundleResources(@RequestParam("files") MultipartFile[] files) throws IOException {
+        writeDocumentToServer(files);
+        return "success";
+    }
+
+    private String writeDocumentToServer(MultipartFile[] files) throws IOException, FileNotFoundException {
+        String msg = "";
+        for (int i = 0; i < files.length; i++) {
+            String fileName = files[i].getOriginalFilename();
+
+            byte[] bytes = files[i].getBytes();
+            File newUploadFile = new File(UPLOADED_FOLDER + fileName);
+            BufferedOutputStream buffStream = new BufferedOutputStream(new FileOutputStream(newUploadFile));
+            buffStream.write(bytes);
+            buffStream.close();
+
+           /* if (StringUtils.isNotEmpty(category)) {
+                FileUtils.deleteQuietly(newUploadFile);
+            }*/
+
+            File directory = new File(UPLOADED_FOLDER);
+            if (!directory.exists()) {
+                directory.mkdir();
+            }
+
+            BufferedOutputStream buffStreamCate = new BufferedOutputStream(
+                    new FileOutputStream(new File(UPLOADED_FOLDER + "/" + fileName)));
+            buffStreamCate.write(bytes);
+
+            buffStreamCate.close();
+        }
+        msg = "You have successfully uploaded.";
+        return msg;
     }
 
 
