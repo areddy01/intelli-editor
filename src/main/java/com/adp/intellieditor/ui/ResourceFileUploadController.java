3da0ef1779cf4a7c62209631f7df6cbb77aa3852
Aditya Reddy <adityaravikiran.reddy@adp.com>
2017-06-19 19:03:26 +0530

    Adding Upload controller & CORS filter .

---
+++ src/main/java/com/adp/intellieditor/ui/ResourceFileUploadController.java
@@ -0,0 +1,24 @@
+package com.adp.intellieditor.ui;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Created by reddyadi on 6/19/2017.
+ */
+
+@RestController
+@RequestMapping("/upload")
+public class ResourceFileUploadController {
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String multiFileUpload(@RequestParam("files") MultipartFile[] files) {
+        return "";
+    }
+
+
+}
